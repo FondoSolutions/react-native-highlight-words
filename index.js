f350d5c512b996a95c214c236a3bafe01a5fffd3
Simon Mattsson <simonarnemattsson@gmail.com>
2022-09-21 08:59:27 +0200

    updated proptypes

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {findAll} from 'highlight-words-core';
+import { Text } from 'react-native';
+import { findAll } from 'highlight-words-core';
 import PropTypes from 'prop-types';
+import { TextPropTypes } from 'deprecated-react-native-prop-types';
 
 Highlighter.propTypes = {
-    autoEscape: PropTypes.bool,
-    highlightStyle: Text.propTypes.style,
-    searchWords: PropTypes.arrayOf(PropTypes.string).isRequired,
-    textToHighlight: PropTypes.string.isRequired,
-    sanitize: PropTypes.func,
-    style: Text.propTypes.style
+  autoEscape: PropTypes.bool,
+  highlightStyle: Text.propTypes.style,
+  searchWords: PropTypes.arrayOf(PropTypes.string).isRequired,
+  textToHighlight: PropTypes.string.isRequired,
+  sanitize: PropTypes.func,
+  style: TextPropTypes.style
 };
 
 /**
@@ -17,32 +18,32 @@ Highlighter.propTypes = {
 * This function returns an array of strings and <Text> elements (wrapping highlighted words).
 */
 export default function Highlighter({
-    autoEscape,
-    highlightStyle,
-    searchWords,
-    textToHighlight,
-    sanitize,
-    style,
-    ...props
+  autoEscape,
+  highlightStyle,
+  searchWords,
+  textToHighlight,
+  sanitize,
+  style,
+  ...props
 }) {
-    const chunks = findAll({textToHighlight, searchWords, sanitize, autoEscape});
+  const chunks = findAll({ textToHighlight, searchWords, sanitize, autoEscape });
 
-    return (
-        <Text style={style} {...props}>
-            {chunks.map((chunk, index) => {
-                const text = textToHighlight.substr(chunk.start, chunk.end - chunk.start);
+  return (
+    <Text style={style} {...props}>
+      {chunks.map((chunk, index) => {
+        const text = textToHighlight.substr(chunk.start, chunk.end - chunk.start);
 
-                return (!chunk.highlight)
-                    ? text
-                    : (
-                        <Text
-                            key={index}
-                            style={chunk.highlight && highlightStyle}
-                        >
-                            {text}
-                        </Text>
-                    );
-            })}
-        </Text>
-    );
+        return (!chunk.highlight)
+          ? text
+          : (
+            <Text
+              key={index}
+              style={chunk.highlight && highlightStyle}
+            >
+              {text}
+            </Text>
+          );
+      })}
+    </Text>
+  );
 }
